a1c4bab6d67acb555d4b98ea8cbacc27e2955fac
weiyang0 <nwy290@gmail.com>
2020-12-24 12:46:07 +0800

    FooterContainer

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,12 @@
 import React from "react"
-import {JumbotronContainer} from '../containers/jumbotron'
+import { FooterContainer } from "../containers/footer"
+import { JumbotronContainer } from '../containers/jumbotron'
 
 export default function Home() {
     return (
-        <JumbotronContainer />
+        <>
+            <JumbotronContainer />
+            <FooterContainer />
+        </>
     )
 }
