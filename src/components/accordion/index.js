338999007da053dccda56dada642d699f91b6681
weiyang0 <nwy290@gmail.com>
2020-12-25 22:12:20 +0800

    Style Accordion component

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Container, Inner, Item, Title, Body, Header } from './styles/accordion'
+import { Container, Inner, Item, Title, Body, Header, Frame } from './styles/accordion'
 
 export default function Accordion({ children, ...restProps }) {
     return (
@@ -7,6 +7,10 @@ export default function Accordion({ children, ...restProps }) {
             <Inner>{children}</Inner>
         </Container>
     )
+}
+
+Accordion.Frame = function AccordionFrame({ children, ...restProps }) {
+    return <Frame {...restProps}>{children}</Frame>;
 }
 
 Accordion.Item = function AccordionItem({ children, ...restProps }) {
