0ef45f5c582033eee21872fbc38db0bc56d36121
weiyang0 <nwy290@gmail.com>
2020-12-26 15:09:37 +0800

    Style OptForm component

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -16,7 +16,11 @@ export function FaqsContainer() {
             </Accordion.Frame>
 
             <OptForm>
-                    <OptForm.Input placeholder="Email Address" />
+                <OptForm.Input placeholder="Email Address" />
+                <OptForm.Button>Try it now</OptForm.Button>
+                <OptForm.Break />
+                <OptForm.Text>Ready to watch? Enter your email to create or restart
+                your membership.</OptForm.Text>
             </OptForm>
         </Accordion>
     )
