e9b8a9f8e566c8ec3eb4d70793570a3bff3d6dc1
weiyang0 <nwy290@gmail.com>
2020-12-28 14:54:52 +0800

    Style Form component

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -4,6 +4,12 @@ import { HeaderContainer } from '../containers/header'
 
 export default function Signin() {
     const [error, setError] = useState('')
+    const [emailAddress, setEmailAddress] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSignin = (event) => {
+        event.preventDefault()
+    }
 
     return (
         <>
@@ -11,6 +17,31 @@ export default function Signin() {
                 <Form>
                     <Form.Title>Sign In</Form.Title>
                     {error && <Form.Error>{error}</Form.Error>}
+
+                    <Form.Base onSubmit={handleSignin} method="POST">
+                        <Form.Input 
+                            placeholder="Email address"
+                            value={emailAddress}
+                            onChange={({ target }) => setEmailAddress(target.value)}
+                        />
+                        <Form.Input 
+                            type="password"
+                            autoComplete="off"
+                            placeholder="Password"
+                            value={password}
+                            onChange={({ target }) => setPassword(target.value)}
+                        />
+                        <Form.Submit disabled={false} type="submit">
+                            Sign In
+                        </Form.Submit>
+                        
+                        <Form.Text>
+                            New to Netflix? <Form.Link to="/signup">Sign up</Form.Link>
+                        </Form.Text>
+                        <Form.TextSmall>
+                            This page is protected by Google reCAPTCHA
+                        </Form.TextSmall>
+                    </Form.Base>
                 </Form>
             </HeaderContainer>
         </>
