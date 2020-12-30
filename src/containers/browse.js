12572347508816d2abb427943ed5dc3b415abec8
weiyang0 <nwy290@gmail.com>
2020-12-31 00:58:13 +0800

    Build Browse page and BrowseContainer

---
+++ src/containers/browse.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import { Header } from '../components'
+import * as ROUTES from '../constants/routes'
+import { FirebaseContext } from '../context/firebase'
+import { SelectProfileContainer } from './profiles'
+import { FooterContainer } from './footer'
+
+export function BrowseContainer() {
+    const [profile, setProfile] = useState({})
+    
+    const user = {
+        displayName: "LP",
+        photoURL: "1"
+    }
+
+    return profile.displayName ? (
+        <>
+            <p>Browse Container</p>
+            <FooterContainer />
+        </>)
+        :
+        (<>
+            <SelectProfileContainer />
+        </>)
+}
