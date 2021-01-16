89520db5f6d128bf04c6588a76ed9be49301bc82
weiyang0 <nwy290@gmail.com>
2021-01-16 22:14:28 +0800

    Build Card component

---
+++ src/components/card/styles/card.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components'
+
+
+export const Title = styled.p`
+
+`
+
+export const Container = styled.div`
+
+`
+
+export const Group = styled.div`
+
+`
+
+export const SubTitle = styled.p`
+
+`
+
+export const Text = styled.p`
+
+`
+
+export const Entities = styled.div`
+
+`
+
+export const Meta = styled.div`
+
+`
+
+export const Image = styled.img`
+
+`
+
+export const Item = styled.div`
+
+`
+
+export const FeatureText = styled.p`
+    
+`
+
+export const Feature = styled.div`
+
+`
+
+export const FeatureTitle = styled.h1`
+    
+`
+
+export const FeatureClose = styled.button`
+    
+`
+
+export const Content = styled.p`
+    
+`
+
+export const Maturity = styled.div`
+    
+`
+
+// Container, Group, Title, SubTitle, Text, Entities,
+// Meta, Item, Image, Feature, FeatureTitle,
+// FeatureClose, Maturity
