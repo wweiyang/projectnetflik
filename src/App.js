c12b040a297044f6ce6a4a1191bcb06bdf94c4af
weiyang0 <nwy290@gmail.com>
2021-01-20 21:40:33 +0800

    Build custom useAuthListener hook

--- src/App.js
+++ src/App.js
@@ -3,25 +3,28 @@ import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import { Route } from 'react-router-dom'
 import * as ROUTES from './constants/routes'
 import { Browse, Home, Signin, Signup } from './pages'
+import { useAuthListener } from './hooks'
 
 export function App() {
-  return (
-    <Router basename={process.env.PUBLIC_URL}>
-        <Switch>
-            <Route path={ROUTES.SIGN_IN}>
-                <Signin />
-            </Route>
-            <Route path={ROUTES.SIGN_UP}>
-                <Signup />
-            </Route>
-            <Route path={ROUTES.BROWSE}>
-                <Browse />
-            </Route>
-            <Route path={ROUTES.HOME}>
-                <Home />
-            </Route>
-        </Switch>
-    </Router>
+    const { user } = useAuthListener()
+
+    return (
+        <Router basename={process.env.PUBLIC_URL}>
+            <Switch>
+                <Route path={ROUTES.SIGN_IN}>
+                    <Signin />
+                </Route>
+                <Route path={ROUTES.SIGN_UP}>
+                    <Signup />
+                </Route>
+                <Route path={ROUTES.BROWSE}>
+                    <Browse />
+                </Route>
+                <Route path={ROUTES.HOME}>
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
   )
 }
 
