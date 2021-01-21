a6479c9ab17adf796b71a6dcc09dca8759170e5c
weiyang0 <nwy290@gmail.com>
2021-01-21 09:41:30 +0800

    Remove unused code

--- src/index.js
+++ src/index.js
@@ -5,17 +5,6 @@ import { App } from './app';
 import { firebase } from './lib/firebase.prod'
 import { FirebaseContext } from './context/firebase'
 
-// const config = {
-//     apiKey: "AIzaSyCaYGHT9BFwZLOrpgWS1eHsuN6IwC81918",
-//     authDomain: "project-netflik.firebaseapp.com",
-//     projectId: "project-netflik",
-//     storageBucket: "project-netflik.appspot.com",
-//     messagingSenderId: "427449280605",
-//     appId: "1:427449280605:web:3e1229268202b206a105b4"
-// }
-
-// const firebase = window.firebase.initializeApp(config)
-
 ReactDOM.render(
     <>
         <FirebaseContext.Provider value={{ firebase }}>
